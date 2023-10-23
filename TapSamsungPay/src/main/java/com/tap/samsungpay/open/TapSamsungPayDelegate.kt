e63ed9ec0cc7296c5c24d17594f6e902d61f323c
AhlaamK-tap <a.kazi@tap.company>
2023-10-23 15:35:40 +0300

    refc

--- TapSamsungPay/src/main/java/com/tap/samsungpay/open/TapSamsungPayDelegate.kt
+++ TapSamsungPay/src/main/java/com/tap/samsungpay/open/TapSamsungPayDelegate.kt
@@ -1,3 +1,10 @@
+/**
+ * Created by $AhlaamK on 10/23/23, 3:30 PM
+ * Copyright (c) 2023 .
+ * Tap Payments All rights reserved.
+ **
+ */
+
 package com.tap.samsungpay.open
 
 import com.tap.samsungpay.internal.api.responses.Token
